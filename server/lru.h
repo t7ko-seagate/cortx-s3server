95806d00401d2e8810011095f3ba8c0e482b36d9
Ivan Tishchenko <ivan.tishchenko@seagate.com>
2020-12-21 10:06:02 -0700

    Fix formatting to comply with CLANG rules
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

--- server/lru.h
+++ server/lru.h
@@ -11,52 +11,52 @@
 
 using namespace std;
 
-template<typename key, typename val> class lru {
-	int                                                         size;
-	list<key>                                                   queue;
-	unordered_map<key, pair<val, typename list<key>::iterator>> map;
-public:
-	void reclaim() {
-		while (map.size() >= size) {
-			map.erase(queue.back());
-			queue.pop_back();
-		}
-	}
+template <typename key, typename val>
+class lru {
+  int size;
+  list<key> queue;
+  unordered_map<key, pair<val, typename list<key>::iterator>> map;
 
-	lru(int s) {
-		size = s;
-	}
+ public:
+  void reclaim() {
+    while (map.size() >= size) {
+      map.erase(queue.back());
+      queue.pop_back();
+    }
+  }
 
-	void put(const key k, const val v) {
-		auto it = map.find(k);
-		reclaim();
-		if (it != map.end()) {
-			queue.erase(it->second.second);
-		}
-		queue.push_front(k);
-		map[k] = { v, queue.begin() };
-	}
+  lru(int s) { size = s; }
 
-	val get(const key k) {
-		auto it = map.find(k);
-		if (it != map.end()) {
-			queue.erase(it->second.second);
-			queue.push_front(k);
-			map[k] = { it->second.first, queue.begin() };
-			return it->second.first;
-		} else {
-			return val();
-		}
-	}
-	friend ostream &operator<<(ostream &out, const lru<key, val> &self) {
-		string sep = "";
-		out << "[";
-		for (auto const& k : self.queue) {
-			out << sep << k << " : " << self.map.find(k)->first;
-			sep = ", ";
-		}
-		return out << "]";
-	}
+  void put(const key k, const val v) {
+    auto it = map.find(k);
+    reclaim();
+    if (it != map.end()) {
+      queue.erase(it->second.second);
+    }
+    queue.push_front(k);
+    map[k] = {v, queue.begin()};
+  }
+
+  val get(const key k) {
+    auto it = map.find(k);
+    if (it != map.end()) {
+      queue.erase(it->second.second);
+      queue.push_front(k);
+      map[k] = {it->second.first, queue.begin()};
+      return it->second.first;
+    } else {
+      return val();
+    }
+  }
+  friend ostream &operator<<(ostream &out, const lru<key, val> &self) {
+    string sep = "";
+    out << "[";
+    for (auto const &k : self.queue) {
+      out << sep << k << " : " << self.map.find(k)->first;
+      sep = ", ";
+    }
+    return out << "]";
+  }
 };
 
 /* __S3_SERVER_LRU_H__ */
